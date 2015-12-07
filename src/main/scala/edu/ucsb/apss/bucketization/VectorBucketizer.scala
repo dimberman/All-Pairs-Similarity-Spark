bf00f6a980531a1033cfed1ae37c675b1fedca47
Daniel Imberman <daniel.imberman@hgdata.com>
2015-12-06 23:44:08 -0800

    initial commit

---
+++ src/main/scala/edu/ucsb/apss/bucketization/VectorBucketizer.scala
@@ -0,0 +1,11 @@
+package edu.ucsb.apss.bucketization
+
+import org.apache.spark.mllib.linalg.SparseVector
+
+
+/**
+  * Created by dimberman on 12/6/15.
+  */
+class VectorBucketizer(val anchors:List[SparseVector]) extends Serializable{
+
+}
---
+++ src/test/scala/edu/ucsb/apss/bucketization/VectorBucketizerTest.scala
@@ -0,0 +1,21 @@
+package edu.ucsb.apss.bucketization
+
+import org.scalatest.{BeforeAndAfter, FlatSpec}
+
+/**
+  * Created by dimberman on 12/6/15.
+  */
+class VectorBucketizerTest extends FlatSpec with BeforeAndAfter{
+
+  "The Bucketizer" should "read in a number of vectors and turn them into 'anchor points'" in {
+
+  }
+
+  //TODO how the hell do I get this class to take in both?
+  "The Bucketizer" should "only be able to take in either sparse vectors or dense vectors" in {
+
+  }
+
+
+
+}
